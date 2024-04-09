362ba4629bddb9a62673c8def99be1adca90f09d
DipuTut <mddipu@gmail.com>
2024-04-09 13:17:29 +0200

    .\.git\

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 import inquirer from 'inquirer';
+import qr from 'qr-image';
+import fs from 'fs';
+
 
 inquirer
   .prompt([
@@ -7,12 +10,20 @@ inquirer
         message: "Gebe eine URL hier ein: ",
         name: "URL"
     }
-
   ])
   .then((answers) => {
     // Use user feedback for... whatever!!
     const url = answers.URL
     console.log(url)
+
+    var qr_png = qr.image(url);
+    qr_png.pipe(fs.createWriteStream('qr_code.png'));
+
+    fs.writeFile('url.txt', url, (err) => {
+        if (err) throw err;
+        console.log('The file has been saved!');
+      }); 
+
   })
   .catch((error) => {
     if (error.isTtyError) {
---
+++ synchron_asynchron/asynchron_callback.js
@@ -0,0 +1,6 @@
+console.log("Erste Aufgabe")
+setTimeout(() => {
+    console.log("Zweite Aufgabe")
+}, 2000)
+
+console.log("Dritte Aufgabe")
